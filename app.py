1957e91a8c9539902ecf2f48e544db1c6d2bbee7
Taha Lewis <tahalewis@Tahas-Air.attlocal.net>
2023-09-15 16:14:51 -0500

    m

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
+import os
 
-app = Flask(__name__, template_folder="templates")
+app = Flask(__name__, template_folder="react-app/build")
 
-@app.route('/money')
+# Serve the index.html file
+@app.route('/')
 def index():
-    return render_template('index.html') 
+    return render_template('index.html')
+
+# Serve static files from the 'build' directory
+@app.route('/static/<path:path>')
+def serve_static(path):
+    static_folder = os.path.join(os.getcwd(), 'react-app/build/static')
+    return send_from_directory(static_folder, path)
 
 if __name__ == '__main__':
     app.run(debug=True)
