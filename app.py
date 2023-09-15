5a4a40bc6d06e527a881a7874a340896e28e8626
Taha Lewis <tahalewis@Tahas-Air.attlocal.net>
2023-09-15 12:52:35 -0500

    file struc

--- app.py
+++ app.py
@@ -3,10 +3,8 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 # @app.route('/api/data', methods=['GET'])
-
-@app.route('/')
 def index():
-    return render_template("Home.js")
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run()
