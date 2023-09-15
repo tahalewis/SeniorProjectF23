bdc577fa41a0cd889698227fe83c26488dc13a3a
Taha Lewis <tahalewis@Tahas-Air.attlocal.net>
2023-09-15 15:10:37 -0500

    m

--- app.py
+++ app.py
@@ -2,15 +2,10 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/money')
+@app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('/templates/index.html')
 
-# @app.route('/api/data')
-# def get_data():
-#     # Your API logic here
-#     data = {'message': 'Hello from Flask API!'}
-#     return jsonify(data)
 
 if __name__ == '__main__':
     app.run(debug=True)
