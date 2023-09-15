80869d8ccd01e2015f22a6b9d4182bf39eab8d2d
garick-git <73561186+Renzoe-Box@users.noreply.github.com>
2023-09-15 11:53:57 -0500

    frontend push

--- backend/app.py
+++ backend/app.py
@@ -2,9 +2,11 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/api/data', methods=['GET'])
+# @app.route('/api/data', methods=['GET'])
+
+@app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template("Home.js")
 
 if __name__ == '__main__':
     app.run()
