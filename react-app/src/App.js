cfb62241e056fbaedfedde9733420f7006bcecf7
Taha Lewis <tahalewis@Tahas-Air.attlocal.net>
2023-09-15 17:11:08 -0500

    d

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,25 +1,23 @@
-import React, { useState, useEffect } from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const [message, setMessage] = useState('');
-  const [error, setError] = useState('');
-
-  useEffect(() => {
-    fetch('/api')
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        return response.json();
-      })
-      .then(data => setMessage(data.message))
-      .catch(error => setError(error.message));
-  }, []);
-
   return (
-    <div>
-      {error && <p>{error}</p>}
-      <h1>{message}</h1>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
