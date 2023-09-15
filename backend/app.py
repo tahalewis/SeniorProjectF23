4ef071b88edb83c92db7586dfe9d0453b5f7242b
Taha Lewis <tahalewis@Tahas-Air.attlocal.net>
2023-09-14 23:40:30 -0500

    added app.py'

--- backend/app.py
+++ backend/app.py
@@ -1,41 +1,10 @@
-from flask import Flask, jsonify, request
-from flask_cors import CORS
-import os
+from flask import Flask, render_template
 
 app = Flask(__name__)
-CORS(app)  # Enable Cross-Origin Resource Sharing
-
-# Serve React build files (replace 'frontend/build' with your actual build path)
-frontend_build_path = os.path.join(os.path.dirname(__file__), 'frontend', 'build')
 
 @app.route('/')
-def serve_index():
-    return app.send_static_file('index.html')
-
-@app.route('/<path:filename>')
-def serve_static(filename):
-    return app.send_static_file(filename)
-
-# Sample data (replace with your data source)
-data = [
-    {"id": 1, "name": "Item 1"},
-    {"id": 2, "name": "Item 2"},
-    {"id": 3, "name": "Item 3"},
-]
-
-@app.route('/api/items', methods=['GET'])
-def get_items():
-    return jsonify(data)
-
-@app.route('/api/items', methods=['POST'])
-def add_item():
-    new_item = request.json
-    if new_item:
-        new_item["id"] = len(data) + 1
-        data.append(new_item)
-        return jsonify({"message": "Item added successfully"}), 201
-    else:
-        return jsonify({"message": "Invalid data"}), 400
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
