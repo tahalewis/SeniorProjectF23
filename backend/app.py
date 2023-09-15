52f338b9f38eccb8f8434fd2459aa03ebe808ea5
Taha Lewis <tahalewis@x-10-5-46-26.wifi.stedwards.edu>
2023-09-15 11:38:50 -0500

    m

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,10 @@
-from flask import Flask, jsonify
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/api/data', methods=['GET'])
-def get_data():
-    data = {'message': 'Hello from the Python backend!'}
-    return jsonify(data)
+def index():
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run()
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,20 @@
-// src/App.js
-
-import React from 'react';
-import './App.css';
-import ItemList from './components/ItemList';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    // Make an HTTP request to the Python backend
+    fetch('/api/data')
+      .then(response => response.json())
+      .then(result => setData(result))
+      .catch(error => console.error(error));
+  }, []);
+
   return (
     <div className="App">
-      <ItemList />
+      <h1>Hello from React</h1>
+      <p>Message from Python: {data.message}</p>
     </div>
   );
 }
