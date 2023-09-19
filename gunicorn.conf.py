24d7f2675d992c9c6236b51d254456e66c8c34f5
Taha Lewis <tahalewis@Tahas-Air.attlocal.net>
2023-09-18 21:35:23 -0500

    s

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -4,3 +4,6 @@ bind = '0.0.0.0:8000'  # Your binding address
 workers = 3  # Number of Gunicorn worker processes
 errorlog = os.path.join(os.getcwd(), 'gunicorn_error.log')
 accesslog = os.path.join(os.getcwd(), 'gunicorn_access.log')
+
+
+app = "app:app"
