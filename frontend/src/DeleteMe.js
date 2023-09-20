a1c38cbceebf334a1a7bb17bd1ea38be3dc54f12
Taha Lewis <tahalewis@Tahas-Air.attlocal.net>
2023-09-19 19:35:20 -0500

    g

--- frontend/src/DeleteMe.js
+++ frontend/src/DeleteMe.js
@@ -1,7 +1,28 @@
+import React from 'react';
+
 const DeleteMe = () => {
-    return(
-        <div>DELETE ME DADDY!</div>
-    );
-}
+  return (
+    <div className="deleteMeContainer">
+      <h2>Delete Me Daddy!</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vel
+        velit non felis dapibus pulvinar. Integer ut diam nec sapien feugiat
+        volutpat. Vivamus cursus hendrerit ante, id lacinia urna venenatis vel.
+      </p>
+      <ul>
+        <li>Item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
+      </ul>
+      <p>
+        Fusce ullamcorper tortor in sem condimentum, id congue quam feugiat.
+        Sed varius sem vel eros tincidunt, sit amet varius tortor vehicula.
+        Phasellus volutpat, nunc eget congue eleifend, est turpis eleifend
+        tortor.
+      </p>
+      <button className="deleteButton">Delete</button>
+    </div>
+  );
+};
 
 export default DeleteMe;
