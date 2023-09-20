827defa84a89df834f2c66197929d2351035b4b1
Taha Lewis <tahalewis@Tahas-Air.attlocal.net>
2023-09-19 19:28:41 -0500

    g

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,19 +5,11 @@ import Home from './Home';
 import DeleteMe from './DeleteMe';
 
 function App() {
-  const navigate = useNavigate();
-
-  // You can add a function to handle the '/' route redirection
-  const redirectToHome = () => {
-    navigate('/home');
-  };
-
   return (
     <Router>
       <div className='appDiv'>
         <Routes>
-          <Route path="/" element={redirectToHome} />
-          <Route path="/home" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="/delete-me-page" element={<DeleteMe />} />
         </Routes>
       </div>
