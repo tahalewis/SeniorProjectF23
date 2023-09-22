eab4e200e17b6f5d19a09ed4097cd00009e3b9d7
Taha Lewis <tahalewis@Tahas-Air.attlocal.net>
2023-09-22 14:03:24 -0500

    f

--- frontend/src/PlayerSearch.js
+++ frontend/src/PlayerSearch.js
@@ -1,68 +1,50 @@
-  // render() {
-  //   return (
-  //     <div>
-  //       <h1>NBA Player Search</h1>
-  //       <input
-  //         type="text"
-  //         placeholder="Search for players by name"
-  //         value={this.state.searchInput}
-  //         onChange={this.handleInputChange}
-  //       />
-  //       <button onClick={this.searchPlayers}>Search</button>
-  //       <ul>
-  //         {this.state.players.map((player, index) => (
-  //           <li key={index}>{player.full_name}</li>
-  //         ))}
-  //       </ul>
-  //     </div>
-  //   );
-  // }
+import React, { useState, useEffect } from 'react';
 
+const PlayerSearch = () => {
+  const [searchInput, setSearchInput] = useState('');
+  const [players, setPlayers] = useState([]);
 
-
-  import React, { useState, useEffect } from 'react';
-
-  const PlayerSearch = () => {
-    const [searchInput, setSearchInput] = useState('');
-    const [players, setPlayers] = useState([]);
-  
-    const handleInputChange = (e) => {
-      setSearchInput(e.target.value);
-    };
-  
-    useEffect(() => {
-      if (searchInput.trim() === '') {
-        setPlayers([]); // Clear the players list when the input is empty.
-        return;
-      }
-  
-      fetch(`/api/player/search/${searchInput}`)
-        .then((response) => response.json())
-        .then((data) => {
-          setPlayers(data);
-        })
-        .catch((error) => {
-          console.error('Error:', error);
-        });
-    }, [searchInput]); // This effect should run whenever searchInput changes.
-  
-    return (
-      <div>
-        <h1>NBA Player Search</h1>
-        <input
-          type="text"
-          placeholder="Search for players by name"
-          value={searchInput}
-          onChange={handleInputChange}
-        />
-        <button onClick={() => {}}>Search</button>
-        <ul>
-          {players.map((player, index) => (
-            <li key={index}>{player.full_name}</li>
-          ))}
-        </ul>
-      </div>
-    );
+  const handleInputChange = (e) => {
+    setSearchInput(e.target.value);
   };
-  
-  export default PlayerSearch;
+
+  const searchPlayers = () => {
+    if (searchInput.trim() === '') {
+      setPlayers([]);
+      return;
+    }
+
+    fetch(`/api/player/search/${searchInput}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setPlayers(data);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  };
+
+  useEffect(() => {
+    searchPlayers(); // Initially fetch data when the component mounts.
+  }, []); // An empty dependency array means this effect runs once after the initial render.
+
+  return (
+    <div>
+      <h1>NBA Player Search</h1>
+      <input
+        type="text"
+        placeholder="Search for players by name"
+        value={searchInput}
+        onChange={handleInputChange}
+      />
+      <button onClick={searchPlayers}>Search</button>
+      <ul>
+        {players.map((player, index) => (
+          <li key={index}>{player.full_name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default PlayerSearch;
