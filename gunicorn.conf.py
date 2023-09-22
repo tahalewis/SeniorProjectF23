13aab40da317b60b9d58ed3df39917f8d16611be
Taha Lewis <tahalewis@Tahas-Air.attlocal.net>
2023-09-22 12:08:09 -0500

    f

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -6,8 +6,8 @@ workers = 3  # Number of Gunicorn worker processes
 errorlog = os.path.join(os.getcwd(), 'gunicorn_error.log')
 accesslog = os.path.join(os.getcwd(), 'gunicorn_access.log')
 
-# gunicorn.conf.py
-pythonpath = '/root/SeniorProjectF23/venv/bin/python'
+# # gunicorn.conf.py
+# pythonpath = '/root/SeniorProjectF23/venv/bin/python'
 
 sys.path.append('/root/seniorProjectF23')
 app = "app:app"
