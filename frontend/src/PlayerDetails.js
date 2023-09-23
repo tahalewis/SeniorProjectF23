b0abce212e63451ca922728a6a231bdd5d6b02bb
Taha Lewis <tahalewis@Tahas-Air.attlocal.net>
2023-09-23 11:03:52 -0500

    f

--- frontend/src/PlayerDetails.js
+++ frontend/src/PlayerDetails.js
@@ -1,28 +1,36 @@
 import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
 
-const PlayerDetails = () => {
-  const { playerId } = useParams();
-  const [playerInfo, setPlayerInfo] = useState({});
+const PlayerDetails = ({ playerId }) => {
+  const [playerDetails, setPlayerDetails] = useState(null);
 
   useEffect(() => {
-    // Fetch player details based on the playerId from the route parameter
-    fetch(`/api/playerinfo/id/${playerId}`)
-      .then((response) => response.json())
-      .then((data) => {
-        setPlayerInfo(data);
-      })
-      .catch((error) => {
-        console.error('Error:', error);
-      });
+    // Fetch player details based on playerId
+    if (playerId) {
+      fetch(`/api/playerinfo/id/${playerId}`)
+        .then((response) => response.json())
+        .then((data) => {
+          setPlayerDetails(data); // Set player details data
+        })
+        .catch((error) => {
+          console.error('Error:', error);
+        });
+    }
   }, [playerId]);
 
+  if (!playerDetails) {
+    // Loading state
+    return <div>Loading...</div>;
+  }
+
+  // Render player details
   return (
     <div className="PlayerDetails">
       <h1>Player Details</h1>
-      <p>Name: {playerInfo.full_name}</p>
-      <p>Team: {playerInfo.team_name}</p>
-      {/* Add more player details as needed */}
+      <div>
+        <h2>{playerDetails.full_name}</h2>
+        <p>Birthdate: {playerDetails.birthdate}</p>
+        {/* Add more player details here */}
+      </div>
     </div>
   );
 };
