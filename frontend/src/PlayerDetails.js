3924543e2db568885207afeff33d27f5b9cc1043
Taha Lewis <tahalewis@Tahas-Air.attlocal.net>
2023-09-23 00:41:32 -0500

    g

--- frontend/src/PlayerDetails.js
+++ frontend/src/PlayerDetails.js
@@ -1,49 +1,29 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import './PlayerDetails.css';
-
 
 const PlayerDetails = () => {
-  const { playerName } = useParams();
-  const [playerInfo, setPlayerInfo] = useState(null);
+  const { playerId } = useParams();
+  const [playerInfo, setPlayerInfo] = useState({});
 
   useEffect(() => {
-    if (playerName) {
-      // Fetch player details by name
-      fetch(`/api/player/id/${playerName}`)
-        .then((response) => response.json())
-        .then((data) => {
-          // Assuming the API returns the player ID
-          const playerId = data.id;
+    // Fetch player details using playerId and update state
+    fetch(`/api/playerinfo/id/${playerId}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setPlayerInfo(data); // Update state with player details
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  }, [playerId]);
 
-          // Fetch player details by ID
-          fetch(`/api/playerinfo/id/${playerId}`)
-            .then((response) => response.json())
-            .then((info) => {
-              setPlayerInfo(info);
-            })
-            .catch((error) => {
-              console.error('Error:', error);
-            });
-        })
-        .catch((error) => {
-          console.error('Error:', error);
-        });
-    }
-  }, [playerName]);
-
-  if (!playerInfo) {
-    return <div>Loading...</div>;
-  }
-
+  // Render player details in the JSX
   return (
-    <div>
-      <h2>Player Details</h2>
-      <p>Name: {playerInfo.FIRST_NAME} {playerInfo.LAST_NAME}</p>
-      <p>Birthday: {playerInfo.BIRTHDATE}</p>
-      <p>School: {playerInfo.SCHOOL}</p>
-      <p>Country: {playerInfo.COUNTRY}</p>
-      {/* Add more player information fields here */}
+    <div className="PlayerDetails">
+      <h1>Player Details</h1>
+      <p>Name: {playerInfo.full_name}</p>
+      <p>Team: {playerInfo.team_name}</p>
+      {/* Add more details as needed */}
     </div>
   );
 };
