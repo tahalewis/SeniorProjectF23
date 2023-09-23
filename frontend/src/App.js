34501bc12f4e37670286ea410fed71ecc0eaa7f1
Taha Lewis <tahalewis@Tahas-Air.attlocal.net>
2023-09-22 23:45:14 -0500

    g

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes, useNavigate } from 'react-router-dom';
 import './App.css';
 import Home from './Home';
-import DeleteMe from './DeleteMe';
+import DeleteMe from './PlayerDetails';
 import PlayerSearch from './PlayerSearch';
 
 function App() {
@@ -12,7 +12,7 @@ function App() {
         <Routes>
           <Route path="/playersearch" element={<PlayerSearch />} />
           <Route path="/" element={<Home />} />
-          <Route path="/player/:playerId" component={PlayerDetails} />
+          <Route path="/player/:playerId" element={<PlayerDetails/>} />
         </Routes>
       </div>
     </Router>
