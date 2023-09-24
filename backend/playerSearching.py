7836f6dff7e17a434107e4ca2a1f57007b874c1a
Taha Lewis <tahalewis@Tahas-Air.attlocal.net>
2023-09-24 15:54:10 -0500

    f

--- backend/playerSearching.py
+++ backend/playerSearching.py
@@ -22,7 +22,7 @@ def playerIDFromName(playerName):
     return (playerID[0]['id'])
 
 def getCommonPlayerInfoByID(id):
-    player_info = commonplayerinfo.CommonPlayerInfo(player_id=id, league_id="")
+    player_info = commonplayerinfo.CommonPlayerInfo(id)
     playerJson = player_info.common_player_info.get_dict()
     return playerJson
 
