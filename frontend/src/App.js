3d7b7bb9f8bcf15f4d2f0f794b99b0f43956994c
Taha Lewis <tahalewis@Tahas-Air.attlocal.net>
2023-09-25 18:42:35 -0500

    g

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes, useNavigate } from 'react-router-dom';
 import './App.css';
 import Home from './Home';
-import PlayerDetails from './PlayerDetails';
 import PlayerSearch from './PlayerSearch';
 import InfoPage from './InfoPage';
 
@@ -15,7 +14,6 @@ function App() {
         <Routes>
           <Route path="/playersearch" element={<PlayerSearch />} />
           <Route path="/" element={<Home />} />
-          <Route path="/player/:playerId" element={<PlayerDetails/>} />
           <Route path="/info" element={<InfoPage/>} />
         </Routes>
       </div>
