8d637a087607baf6af7c6f70b6e82e5dcda5ca47
Taha Lewis <tahalewis@Tahas-Air.attlocal.net>
2023-10-01 13:07:37 -0500

    g

--- frontend/src/PlayerSearch.js
+++ frontend/src/PlayerSearch.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './PlayerSearch.css';
+import './index.css';
 
 const PlayerSearch = () => {
   const [searchInput, setSearchInput] = useState('');
