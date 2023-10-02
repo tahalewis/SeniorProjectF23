76f953e485a9ad294f887539d3e6dafc037fa86c
Taha Lewis <tahalewis@Tahas-Air.attlocal.net>
2023-10-02 16:52:00 -0500

    g

--- models/playerStats.py
+++ models/playerStats.py
@@ -5,7 +5,6 @@ from .player import Player
 from .team import Team
 from .game import Game
 
-
 class PlayerStats(db.Model):
     __tablename__ = 'playerStats'
 
@@ -29,12 +28,23 @@ class PlayerStats(db.Model):
     reb = db.Column(db.Integer)
     stl = db.Column(db.Integer)
     turnover = db.Column(db.Integer)
+
+    # Add more columns for additional statistics
+    # Example:
+    # three_pointers_made = db.Column(db.Integer)
+    # three_pointers_attempted = db.Column(db.Integer)
+
+    # ... (other columns)
+
+    # Use ForeignKey references to establish relationships
     player_id = db.Column(db.Integer, db.ForeignKey('players.id'))
     game_id = db.Column(db.Integer, db.ForeignKey('games.id'))
     team_id = db.Column(db.Integer, db.ForeignKey('teams.id'))
-    player = db.relationship('Player', backref='playerStats')
-    game = db.relationship('Game', backref='playerStats')
-    team = db.relationship('Team', backref='playerStats')
+
+    # Define the relationships
+    player = db.relationship('Player', backref='player_stats')
+    game = db.relationship('Game', backref='player_stats')
+    team = db.relationship('Team', backref='player_stats')
 
     @staticmethod
     def fetch_and_insert_stats():
@@ -56,31 +66,41 @@ class PlayerStats(db.Model):
 
                     # Insert data into the player_stats table
                     for stat in stats_data:
-                        player_stat = PlayerStats(
-                            ast=stat.get('ast', 0),
-                            blk=stat.get('blk', 0),
-                            dreb=stat.get('dreb', 0),
-                            fg3_pct=stat.get('fg3_pct', 0.0),
-                            fg3a=stat.get('fg3a', 0),
-                            fg3m=stat.get('fg3m', 0),
-                            fg_pct=stat.get('fg_pct', 0.0),
-                            fga=stat.get('fga', 0),
-                            fgm=stat.get('fgm', 0),
-                            ft_pct=stat.get('ft_pct', 0.0),
-                            fta=stat.get('fta', 0),
-                            ftm=stat.get('ftm', 0),
-                            min=stat.get('min', '0'),
-                            oreb=stat.get('oreb', 0),
-                            pf=stat.get('pf', 0),
-                            pts=stat.get('pts', 0),
-                            reb=stat.get('reb', 0),
-                            stl=stat.get('stl', 0),
-                            turnover=stat.get('turnover', 0),
-                            player_id=stat['player']['id'],
-                            game_id=stat['game']['id'],
-                            team_id=stat['team']['id']
-                        )
-                        db.session.add(player_stat)
+                        player_id = stat['player']['id']
+                        game_id = stat['game']['id']
+                        team_id = stat['team']['id']
+
+                        # Ensure that the referenced records exist in the database
+                        player = Player.query.get(player_id)
+                        game = Game.query.get(game_id)
+                        team = Team.query.get(team_id)
+
+                        if player is not None and game is not None and team is not None:
+                            player_stat = PlayerStats(
+                                ast=stat.get('ast', 0),
+                                blk=stat.get('blk', 0),
+                                dreb=stat.get('dreb', 0),
+                                fg3_pct=stat.get('fg3_pct', 0.0),
+                                fg3a=stat.get('fg3a', 0),
+                                fg3m=stat.get('fg3m', 0),
+                                fg_pct=stat.get('fg_pct', 0.0),
+                                fga=stat.get('fga', 0),
+                                fgm=stat.get('fgm', 0),
+                                ft_pct=stat.get('ft_pct', 0.0),
+                                fta=stat.get('fta', 0),
+                                ftm=stat.get('ftm', 0),
+                                min=stat.get('min', '0'),
+                                oreb=stat.get('oreb', 0),
+                                pf=stat.get('pf', 0),
+                                pts=stat.get('pts', 0),
+                                reb=stat.get('reb', 0),
+                                stl=stat.get('stl', 0),
+                                turnover=stat.get('turnover', 0),
+                                player_id=player_id,
+                                game_id=game_id,
+                                team_id=team_id
+                            )
+                            db.session.add(player_stat)
 
                     db.session.commit()
 
