4bc4e02e914db19527a2af495a47135a3bf983de
Taha Lewis <tahalewis@x-10-5-50-179.wifi.stedwards.edu>
2023-10-02 11:52:18 -0500

    g

--- models/team.py
+++ models/team.py
@@ -1,14 +1,14 @@
-# from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 
-# db = SQLAlchemy()
+db = SQLAlchemy()
 
-# class Team(db.Model):
-#     __tablename__ = 'teams'
+class Team(db.Model):
+    __tablename__ = 'teams'
 
-#     id = db.Column(db.Integer, primary_key=True)
-#     abbreviation = db.Column(db.String(10))
-#     city = db.Column(db.String(50))
-#     conference = db.Column(db.String(20))
-#     division = db.Column(db.String(20))
-#     full_name = db.Column(db.String(100))
-#     name = db.Column(db.String(50))
+    id = db.Column(db.Integer, primary_key=True)
+    abbreviation = db.Column(db.String(10))
+    city = db.Column(db.String(50))
+    conference = db.Column(db.String(20))
+    division = db.Column(db.String(20))
+    full_name = db.Column(db.String(100))
+    name = db.Column(db.String(50))
