0a8a0ea628a01f3fa3012178d68da509c08ec58a
Taha Lewis <tahalewis@Tahas-Air.attlocal.net>
2023-10-10 15:26:15 -0500

    g

--- backend/src/databaseRetrieval/statDBRetrieval.py
+++ backend/src/databaseRetrieval/statDBRetrieval.py
@@ -11,6 +11,7 @@ def getRecentGames(player_id, num_games):
     games = (
         db.session.query(
             Game,
+            PlayerStats,
             func.row_number().over(order_by=Game.date.desc()).label('row_num')
         )
         .join(PlayerStats)
@@ -23,13 +24,34 @@ def getRecentGames(player_id, num_games):
     )
 
     recent_games = []
-    for game, row_num in games:
+    for game, player_stats, row_num in games:
         recent_games.append({
             'date': game.date,
             'home_team': game.home_team.full_name,
             'visitor_team': game.visitor_team.full_name,
             'home_team_score': game.home_team_score,
             'visitor_team_score': game.visitor_team_score,
+            'player_stats': {
+                'ast': player_stats.ast,
+                'blk': player_stats.blk,
+                'dreb': player_stats.dreb,
+                'fg3_pct': player_stats.fg3_pct,
+                'fg3a': player_stats.fg3a,
+                'fg3m': player_stats.fg3m,
+                'fg_pct': player_stats.fg_pct,
+                'fga': player_stats.fga,
+                'fgm': player_stats.fgm,
+                'ft_pct': player_stats.ft_pct,
+                'fta': player_stats.fta,
+                'ftm': player_stats.ftm,
+                'min': player_stats.min,
+                'oreb': player_stats.oreb,
+                'pf': player_stats.pf,
+                'pts': player_stats.pts,
+                'reb': player_stats.reb,
+                'stl': player_stats.stl,
+                'turnover': player_stats.turnover,
+            },
         })
 
     return recent_games
@@ -39,6 +61,7 @@ def getRecentGamesByOpponent(player_id, team_id, num_games):
     games = (
         db.session.query(
             Game,
+            PlayerStats,
             func.row_number().over(order_by=Game.date.desc()).label('row_num')
         )
         .join(PlayerStats)
@@ -54,13 +77,34 @@ def getRecentGamesByOpponent(player_id, team_id, num_games):
     )
 
     recent_games = []
-    for game, row_num in games:
+    for game, player_stats, row_num in games:
         recent_games.append({
             'date': game.date,
             'home_team': game.home_team.full_name,
             'visitor_team': game.visitor_team.full_name,
             'home_team_score': game.home_team_score,
             'visitor_team_score': game.visitor_team_score,
+            'player_stats': {
+                'ast': player_stats.ast,
+                'blk': player_stats.blk,
+                'dreb': player_stats.dreb,
+                'fg3_pct': player_stats.fg3_pct,
+                'fg3a': player_stats.fg3a,
+                'fg3m': player_stats.fg3m,
+                'fg_pct': player_stats.fg_pct,
+                'fga': player_stats.fga,
+                'fgm': player_stats.fgm,
+                'ft_pct': player_stats.ft_pct,
+                'fta': player_stats.fta,
+                'ftm': player_stats.ftm,
+                'min': player_stats.min,
+                'oreb': player_stats.oreb,
+                'pf': player_stats.pf,
+                'pts': player_stats.pts,
+                'reb': player_stats.reb,
+                'stl': player_stats.stl,
+                'turnover': player_stats.turnover,
+            },
         })
 
     return recent_games
