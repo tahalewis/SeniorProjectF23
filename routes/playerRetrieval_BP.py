8b389c65b3dc620675a8e4a8fcdcfc4749836ff8
Taha Lewis <tahalewis@Tahas-Air.attlocal.net>
2023-10-10 16:50:27 -0500

    Added /routes folder so that app.py is smaller

--- routes/playerRetrieval_BP.py
+++ routes/playerRetrieval_BP.py
@@ -1,11 +1,8 @@
-from flask import Blueprint
-from backend.src.models.player import Player
+from flask import Blueprint, jsonify
 from backend.src.databaseRetrieval.playerDBRetrieval import searchPlayerByString
-from backend.src.models.game import Game
-from backend.src.models.playerStats import PlayerStats
 
-playerRetrieval_BP = Blueprint("player_retrieval", __name__)
+databaseRetrieval_BP = Blueprint("db_retrieval", __name__)
 
-@app.route('/api/player/search/<search_input>', methods=['GET'])
+@databaseRetrieval_BP.route('/api/player/search/<search_input>', methods=['GET'])
 def getPlayers(search_input):
     return jsonify(searchPlayerByString(search_input))
