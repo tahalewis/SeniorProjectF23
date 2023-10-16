430013d7fc89dfcaf7e7229a917302aba3d67e6c
Taha Lewis <tahalewis@x-10-5-48-153.wifi.stedwards.edu>
2023-10-16 11:48:31 -0500

    Added new route to search player by id

--- routes/playerRetrieval_BP.py
+++ routes/playerRetrieval_BP.py
@@ -1,8 +1,12 @@
 from flask import Blueprint, jsonify
-from backend.src.databaseRetrieval.playerDBRetrieval import searchPlayerByString
+from backend.src.databaseRetrieval.playerDBRetrieval import searchPlayerByString, get_player_by_id
 
 databaseRetrieval_BP = Blueprint("db_retrieval", __name__)
 
 @databaseRetrieval_BP.route('/api/player/search/<search_input>', methods=['GET'])
 def getPlayers(search_input):
     return jsonify(searchPlayerByString(search_input))
+
+@databaseRetrieval_BP.route('/api/player/search/<player_id>', methods=['GET'])
+def getPlayers(player_id):
+    return jsonify(get_player_by_id(player_id))
