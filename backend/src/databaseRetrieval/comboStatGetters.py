4925c6c1a9fa42cef642787b8f042a6a461f66a8
Taha Lewis <tahalewis@Tahas-Air.attlocal.net>
2023-10-19 17:03:35 -0500

    g

--- backend/src/databaseRetrieval/comboStatGetters.py
+++ backend/src/databaseRetrieval/comboStatGetters.py
@@ -14,10 +14,19 @@ def getAverageAndRecentPRA(player_id, num_games):
     average_points, recent_points = getPointsByNumGames(player_id, num_games)
     average_assists, recent_assists = assistsByNumGames(player_id, num_games)
     average_rebounds, recent_rebounds = reboundsByNumGames(player_id, num_games)
-    
-    average_PRA = round((average_points + average_assists + average_rebounds) / 3, 2)
-    recent_PRA = [round((p + a + r) / 3, 2) for p, a, r in zip(recent_points, recent_assists, recent_rebounds)]
-    
+
+    average_PRA = round(
+        (average_points + average_assists + average_rebounds) / 3, 2
+    )
+
+    if not recent_points or not recent_assists or not recent_rebounds:
+        return [average_PRA, []]
+
+    recent_PRA = [
+        round((p + a + r) / 3, 2)
+        for p, a, r in zip(recent_points, recent_assists, recent_rebounds)
+    ]
+
     return [average_PRA, recent_PRA]
 
 #Points, Rebounds, Assists
@@ -26,72 +35,89 @@ def getAverageAndRecentPRAWithTeam(player_id, team_id, num_games):
     average_points, recent_points = pointsByNumGames_teams(player_id, team_id, num_games)
     average_assists, recent_assists = assistsByNumGames_teams(player_id, team_id, num_games)
     average_rebounds, recent_rebounds = reboundsByNumGames_team(player_id, team_id, num_games)
-    
-    average_PRA = round((average_points + average_assists + average_rebounds) / 3, 2)
-    recent_PRA = [round((p + a + r) / 3, 2) for p, a, r in zip(recent_points, recent_assists, recent_rebounds)]
-    
+
+    if not recent_points or not recent_assists or not recent_rebounds:
+        return [0.0, []]
+
+    average_points = round(average_points, 2)
+    average_assists = round(average_assists, 2)
+    average_rebounds = round(average_rebounds, 2)
+
+    average_PRA = round(
+        (average_points + average_assists + average_rebounds) / 3, 2
+    )
+
+    recent_PRA = [
+        round((p + a + r) / 3, 2)
+        for p, a, r in zip(recent_points, recent_assists, recent_rebounds)
+    ]
+
     return [average_PRA, recent_PRA]
 
-#Rebounds, Assists
-def getAverageAndRecentRA(player_id, num_games):
-    average_assists, recent_assists = assistsByNumGames(player_id, num_games)
-    average_rebounds, recent_rebounds = reboundsByNumGames(player_id, num_games)
-    
-    average_PRA = round((average_assists + average_rebounds) / 3, 2)
-    recent_PRA = [round((a + r) / 2, 2) for a, r in zip(recent_assists, recent_rebounds)]
-    
-    return [average_PRA, recent_PRA]
 
-#Rebounds, Assists
-#WITH TEAM
-def getAverageAndRecentRAWithTeam(player_id, team_id, num_games):
-    average_assists, recent_assists = assistsByNumGames_teams(player_id, team_id, num_games)
-    average_rebounds, recent_rebounds = reboundsByNumGames_team(player_id, team_id, num_games)
-    
-    average_PRA = round((average_assists + average_rebounds) / 2, 2)
-    recent_PRA = [round((a + r) / 2, 2) for a, r in zip(recent_assists, recent_rebounds)]
-    
-    return [average_PRA, recent_PRA]
 
-#Points, Rebounds
-def getAverageAndRecentPR(player_id, num_games):
-    average_points, recent_points = pointsByNumGames_teams(player_id, num_games)
-    average_rebounds, recent_rebounds = reboundsByNumGames(player_id, num_games)
-    
-    average_PRA = round((average_points + average_rebounds) / 3, 2)
-    recent_PRA = [round((a + r) / 2, 2) for a, r in zip(recent_points, recent_rebounds)]
-    
-    return [average_PRA, recent_PRA]
+#Implementation can be further considered later on....
 
-#Points, Rebounds
-#WITH TEAM
-def getAverageAndRecentPRWithTeam(player_id, team_id, num_games):
-    average_points, recent_points = pointsByNumGames_teams(player_id, team_id, num_games)
-    average_rebounds, recent_rebounds = reboundsByNumGames_team(player_id, team_id, num_games)
+# #Rebounds, Assists
+# def getAverageAndRecentRA(player_id, num_games):
+#     average_assists, recent_assists = assistsByNumGames(player_id, num_games)
+#     average_rebounds, recent_rebounds = reboundsByNumGames(player_id, num_games)
     
-    average_PRA = round((average_points + average_rebounds) / 2, 2)
-    recent_PRA = [round((a + r) / 2, 2) for a, r in zip(recent_points, recent_rebounds)]
+#     average_PRA = round((average_assists + average_rebounds) / 3, 2)
+#     recent_PRA = [round((a + r) / 2, 2) for a, r in zip(recent_assists, recent_rebounds)]
     
-    return [average_PRA, recent_PRA]
-#Points, Assists
-def getAverageAndRecentPA(player_id, num_games):
-    average_points, recent_points = pointsByNumGames_teams(player_id, num_games)
-    average_assists, recent_assists = assistsByNumGames_teams(player_id, num_games)
-    
-    average_PRA = round((average_points + average_assists) / 3, 2)
-    recent_PRA = [round((a + r) / 2, 2) for a, r in zip(recent_points, recent_assists)]
-    
-    return [average_PRA, recent_PRA]
+#     return [average_PRA, recent_PRA]
 
-#Points, Assists
-#WITH TEAM
-def getAverageAndRecentPAWithTeam(player_id, team_id, num_games):
-    average_points, recent_points = pointsByNumGames_teams(player_id, team_id, num_games)
-    average_assists, recent_assists = assistsByNumGames_teams(player_id, team_id, num_games)
+# #Rebounds, Assists
+# #WITH TEAM
+# def getAverageAndRecentRAWithTeam(player_id, team_id, num_games):
+#     average_assists, recent_assists = assistsByNumGames_teams(player_id, team_id, num_games)
+#     average_rebounds, recent_rebounds = reboundsByNumGames_team(player_id, team_id, num_games)
     
-    average_PRA = round((average_points + average_assists) / 2, 2)
-    recent_PRA = [round((a + r) / 2, 2) for a, r in zip(recent_points, recent_assists)]
+#     average_PRA = round((average_assists + average_rebounds) / 2, 2)
+#     recent_PRA = [round((a + r) / 2, 2) for a, r in zip(recent_assists, recent_rebounds)]
     
-    return [average_PRA, recent_PRA]
+#     return [average_PRA, recent_PRA]
+
+# #Points, Rebounds
+# def getAverageAndRecentPR(player_id, num_games):
+#     average_points, recent_points = pointsByNumGames_teams(player_id, num_games)
+#     average_rebounds, recent_rebounds = reboundsByNumGames(player_id, num_games)
+    
+#     average_PRA = round((average_points + average_rebounds) / 3, 2)
+#     recent_PRA = [round((a + r) / 2, 2) for a, r in zip(recent_points, recent_rebounds)]
+    
+#     return [average_PRA, recent_PRA]
+
+# #Points, Rebounds
+# #WITH TEAM
+# def getAverageAndRecentPRWithTeam(player_id, team_id, num_games):
+#     average_points, recent_points = pointsByNumGames_teams(player_id, team_id, num_games)
+#     average_rebounds, recent_rebounds = reboundsByNumGames_team(player_id, team_id, num_games)
+    
+#     average_PRA = round((average_points + average_rebounds) / 2, 2)
+#     recent_PRA = [round((a + r) / 2, 2) for a, r in zip(recent_points, recent_rebounds)]
+    
+#     return [average_PRA, recent_PRA]
+# #Points, Assists
+# def getAverageAndRecentPA(player_id, num_games):
+#     average_points, recent_points = pointsByNumGames_teams(player_id, num_games)
+#     average_assists, recent_assists = assistsByNumGames_teams(player_id, num_games)
+    
+#     average_PRA = round((average_points + average_assists) / 3, 2)
+#     recent_PRA = [round((a + r) / 2, 2) for a, r in zip(recent_points, recent_assists)]
+    
+#     return [average_PRA, recent_PRA]
+
+# #Points, Assists
+# #WITH TEAM
+# def getAverageAndRecentPAWithTeam(player_id, team_id, num_games):
+#     average_points, recent_points = pointsByNumGames_teams(player_id, team_id, num_games)
+#     average_assists, recent_assists = assistsByNumGames_teams(player_id, team_id, num_games)
+    
+#     average_PRA = round((average_points + average_assists) / 2, 2)
+#     recent_PRA = [round((a + r) / 2, 2) for a, r in zip(recent_points, recent_assists)]
+    
+#     return [average_PRA, recent_PRA]
 
 
