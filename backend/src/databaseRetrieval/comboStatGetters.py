33360986e3ae18f247a4c77dc13017bc93df8375
Taha Lewis <tahalewis@Tahas-Air.attlocal.net>
2023-10-19 17:11:34 -0500

    g

--- backend/src/databaseRetrieval/comboStatGetters.py
+++ backend/src/databaseRetrieval/comboStatGetters.py
@@ -5,18 +5,24 @@ from sqlalchemy import or_
 from ..models.player import Player
 from ..models.game import Game
 from ..models.playerStats import PlayerStats
-from .astRebStatGetters import assistsByNumGames, assistsByNumGames_teams, reboundsByNumGames, reboundsByNumGames_team
-from .pointStatGetters import getPointsByNumGames, pointsByNumGames_teams
 from database import db
 
-#Points, Rebounds, Assists
-def getAverageAndRecentPRA(player_id, num_games):
-    pra_combo = (
-        db.session.query(
-            func.sum(PlayerStats.pts).label('total_points'),
-            func.sum(PlayerStats.reb).label('total_rebounds'),
-            func.sum(PlayerStats.ast).label('total_assists'),
+def average_and_recent_PRA(player_id, num_games, team_id=None):
+    num_games = int(num_games)
+
+    query = db.session.query(
+        func.sum(PlayerStats.pts).label('total_points'),
+        func.sum(PlayerStats.reb).label('total_rebounds'),
+        func.sum(PlayerStats.ast).label('total_assists')
+    )
+
+    if team_id:
+        query = query.filter(
+            or_(PlayerStats.game.home_team_id == team_id, PlayerStats.game.visitor_team_id == team_id)
         )
+
+    pra_combo = (
+        query
         .join(PlayerStats.game)
         .filter(PlayerStats.player_id == player_id)
         .filter(PlayerStats.min != '00:00')
@@ -30,101 +36,18 @@ def getAverageAndRecentPRA(player_id, num_games):
         return [0.0, []]
 
     total_points, total_rebounds, total_assists = pra_combo[0]
-    average_pra = round(
-        (total_points + total_rebounds + total_assists) / len(pra_combo), 2
-    )
+    average_PRA = round((total_points + total_rebounds + total_assists) / num_games, 2)
 
-    return [average_pra, pra_combo]
+    pra_combo_list = [{
+        'total_points': total_points,
+        'total_rebounds': total_rebounds,
+        'total_assists': total_assists,
+    }]
 
-#Points, Rebounds, Assists
-#WITH TEAM
-def getAverageAndRecentPRAWithTeam(player_id, team_id, num_games):
-    average_points, recent_points = pointsByNumGames_teams(player_id, team_id, num_games)
-    average_assists, recent_assists = assistsByNumGames_teams(player_id, team_id, num_games)
-    average_rebounds, recent_rebounds = reboundsByNumGames_team(player_id, team_id, num_games)
+    return [average_PRA, pra_combo_list]
 
-    if not recent_points or not recent_assists or not recent_rebounds:
-        return [0.0, []]
+def getPRAComboForNumGames(player_id, num_games):
+    return average_and_recent_PRA(player_id, num_games)
 
-    average_points = round(average_points, 2)
-    average_assists = round(average_assists, 2)
-    average_rebounds = round(average_rebounds, 2)
-
-    average_PRA = round(
-        (average_points + average_assists + average_rebounds) / 3, 2
-    )
-
-    recent_PRA = [
-        round((p + a + r) / 3, 2)
-        for p, a, r in zip(recent_points, recent_assists, recent_rebounds)
-    ]
-
-    return [average_PRA, recent_PRA]
-
-
-
-#Implementation can be further considered later on....
-
-# #Rebounds, Assists
-# def getAverageAndRecentRA(player_id, num_games):
-#     average_assists, recent_assists = assistsByNumGames(player_id, num_games)
-#     average_rebounds, recent_rebounds = reboundsByNumGames(player_id, num_games)
-    
-#     average_PRA = round((average_assists + average_rebounds) / 3, 2)
-#     recent_PRA = [round((a + r) / 2, 2) for a, r in zip(recent_assists, recent_rebounds)]
-    
-#     return [average_PRA, recent_PRA]
-
-# #Rebounds, Assists
-# #WITH TEAM
-# def getAverageAndRecentRAWithTeam(player_id, team_id, num_games):
-#     average_assists, recent_assists = assistsByNumGames_teams(player_id, team_id, num_games)
-#     average_rebounds, recent_rebounds = reboundsByNumGames_team(player_id, team_id, num_games)
-    
-#     average_PRA = round((average_assists + average_rebounds) / 2, 2)
-#     recent_PRA = [round((a + r) / 2, 2) for a, r in zip(recent_assists, recent_rebounds)]
-    
-#     return [average_PRA, recent_PRA]
-
-# #Points, Rebounds
-# def getAverageAndRecentPR(player_id, num_games):
-#     average_points, recent_points = pointsByNumGames_teams(player_id, num_games)
-#     average_rebounds, recent_rebounds = reboundsByNumGames(player_id, num_games)
-    
-#     average_PRA = round((average_points + average_rebounds) / 3, 2)
-#     recent_PRA = [round((a + r) / 2, 2) for a, r in zip(recent_points, recent_rebounds)]
-    
-#     return [average_PRA, recent_PRA]
-
-# #Points, Rebounds
-# #WITH TEAM
-# def getAverageAndRecentPRWithTeam(player_id, team_id, num_games):
-#     average_points, recent_points = pointsByNumGames_teams(player_id, team_id, num_games)
-#     average_rebounds, recent_rebounds = reboundsByNumGames_team(player_id, team_id, num_games)
-    
-#     average_PRA = round((average_points + average_rebounds) / 2, 2)
-#     recent_PRA = [round((a + r) / 2, 2) for a, r in zip(recent_points, recent_rebounds)]
-    
-#     return [average_PRA, recent_PRA]
-# #Points, Assists
-# def getAverageAndRecentPA(player_id, num_games):
-#     average_points, recent_points = pointsByNumGames_teams(player_id, num_games)
-#     average_assists, recent_assists = assistsByNumGames_teams(player_id, num_games)
-    
-#     average_PRA = round((average_points + average_assists) / 3, 2)
-#     recent_PRA = [round((a + r) / 2, 2) for a, r in zip(recent_points, recent_assists)]
-    
-#     return [average_PRA, recent_PRA]
-
-# #Points, Assists
-# #WITH TEAM
-# def getAverageAndRecentPAWithTeam(player_id, team_id, num_games):
-#     average_points, recent_points = pointsByNumGames_teams(player_id, team_id, num_games)
-#     average_assists, recent_assists = assistsByNumGames_teams(player_id, team_id, num_games)
-    
-#     average_PRA = round((average_points + average_assists) / 2, 2)
-#     recent_PRA = [round((a + r) / 2, 2) for a, r in zip(recent_points, recent_assists)]
-    
-#     return [average_PRA, recent_PRA]
-
-
+def getPRAComboForNumGamesWithTeam(player_id, team_id, num_games):
+    return average_and_recent_PRA(player_id, num_games, team_id)
