7afef0db650110c9876e861b03358c37a79c4b14
Taha Lewis <tahalewis@Tahas-Air.attlocal.net>
2023-10-19 16:56:26 -0500

    Changed structure of getters

--- backend/src/databaseRetrieval/astRebStatGetters.py
+++ backend/src/databaseRetrieval/astRebStatGetters.py
@@ -7,88 +7,43 @@ from ..models.game import Game
 from ..models.playerStats import PlayerStats
 from database import db
 
-def assistsByNumGames(player_id, num_games):
-    recent_assists = (
-        db.session.query(PlayerStats.ast)
+def average_and_recent_stat(player_id, num_games, stat_column, team_id=None):
+    num_games = int(num_games)
+
+    query = db.session.query(stat_column)
+    
+    if team_id:
+        query = query.filter(
+            (PlayerStats.game.home_team_id == team_id) | (PlayerStats.game.visitor_team_id == team_id)
+        )
+    
+    recent_stats = (
+        query
         .join(PlayerStats.game)
         .filter(PlayerStats.player_id == player_id)
         .filter(PlayerStats.min != '00:00')
         .filter(PlayerStats.min != '00')
-        .order_by(PlayerStats.game.date.desc())
+        .order_by(Game.date.desc())
         .limit(num_games)
         .all()
     )
 
-    if not recent_assists:
+    if not recent_stats:
         return [0.0, []]
 
-    total_assists = sum(assist[0] for assist in recent_assists)
-    average_assists = round(total_assists / num_games, 2)
+    total_stat = sum(stat[0] for stat in recent_stats)
+    average_stat = round(total_stat / num_games, 2)
 
-    return [average_assists, [assist[0] for assist in recent_assists]]
+    return [average_stat, [stat[0] for stat in recent_stats]]
+
+def assistsByNumGames(player_id, num_games):
+    return average_and_recent_stat(player_id, num_games, PlayerStats.ast)
 
 def assistsByNumGames_teams(player_id, team_id, num_games):
-    recent_assists = (
-        db.session.query(PlayerStats.ast)
-        .join(PlayerStats.game)
-        .filter(PlayerStats.player_id == player_id)
-        .filter(PlayerStats.min != '00:00')
-        .filter(PlayerStats.min != '00')
-        .filter(
-            (PlayerStats.game.home_team_id == team_id) | (PlayerStats.game.visitor_team_id == team_id)
-        )
-        .order_by(PlayerStats.game.date.desc())
-        .limit(num_games)
-        .all()
-    )
-
-    if not recent_assists:
-        return [0.0, []]
-
-    total_assists = sum(assist[0] for assist in recent_assists)
-    average_assists = round(total_assists / num_games, 2)
-
-    return [average_assists, [assist[0] for assist in recent_assists]]
+    return average_and_recent_stat(player_id, num_games, PlayerStats.ast, team_id)
 
 def reboundsByNumGames(player_id, num_games):
-    recent_rebounds = (
-        db.session.query(PlayerStats.reb)
-        .join(PlayerStats.game)
-        .filter(PlayerStats.player_id == player_id)
-        .filter(PlayerStats.min != '00:00')
-        .filter(PlayerStats.min != '00')
-        .order_by(PlayerStats.game.date.desc())
-        .limit(num_games)
-        .all()
-    )
-
-    if not recent_rebounds:
-        return [0.0, []]
-
-    total_rebounds = sum(rebound[0] for rebound in recent_rebounds)
-    average_rebounds = round(total_rebounds / num_games, 2)
-
-    return [average_rebounds, [rebound[0] for rebound in recent_rebounds]]
+    return average_and_recent_stat(player_id, num_games, PlayerStats.reb)
 
 def reboundsByNumGames_team(player_id, team_id, num_games):
-    recent_rebounds = (
-        db.session.query(PlayerStats.reb)
-        .join(PlayerStats.game)
-        .filter(PlayerStats.player_id == player_id)
-        .filter(PlayerStats.min != '00:00')
-        .filter(PlayerStats.min != '00')
-        .filter(
-            (PlayerStats.game.home_team_id == team_id) | (PlayerStats.game.visitor_team_id == team_id)
-        )
-        .order_by(PlayerStats.game.date.desc())
-        .limit(num_games)
-        .all()
-    )
-
-    if not recent_rebounds:
-        return [0.0, []]
-
-    total_rebounds = sum(rebound[0] for rebound in recent_rebounds)
-    average_rebounds = round(total_rebounds / num_games, 2)
-
-    return [average_rebounds, [rebound[0] for rebound in recent_rebounds]]
+    return average_and_recent_stat(player_id, num_games, PlayerStats.reb, team_id)
--- backend/src/databaseRetrieval/pointStatGetters.py
+++ backend/src/databaseRetrieval/pointStatGetters.py
@@ -7,49 +7,37 @@ from ..models.game import Game
 from ..models.playerStats import PlayerStats
 from database import db
 
+def average_and_recent_points(player_id, num_games, team_id=None):
+    num_games = int(num_games)
+
+    query = db.session.query(PlayerStats.pts)
+
+    if team_id:
+        query = query.filter(
+            or_(PlayerStats.game.home_team_id == team_id, PlayerStats.game.visitor_team_id == team_id)
+        )
+    
+    recent_points = (
+        query
+        .join(PlayerStats.game)
+        .filter(PlayerStats.player_id == player_id)
+        .filter(PlayerStats.min != '00:00')
+        .filter(PlayerStats.min != '00')
+        .order_by(Game.date.desc())
+        .limit(num_games)
+        .all()
+    )
+
+    if not recent_points:
+        return [0.0, []]
+
+    total_points = sum(point[0] for point in recent_points)
+    average_points = round(total_points / num_games, 2)
+
+    return [average_points, [point[0] for point in recent_points]]
 
 def pointsByNumGames_teams(player_id, team_id, num_games):
-    recent_points = (
-        db.session.query(PlayerStats.pts)
-        .join(PlayerStats.game)
-        .filter(PlayerStats.player_id == player_id)
-        .filter(or_(
-            PlayerStats.game.home_team_id == team_id,
-            PlayerStats.game.visitor_team_id == team_id
-        ))
-        .filter(PlayerStats.min != '00:00')
-        .filter(PlayerStats.min != '00')
-        .order_by(PlayerStats.game.date.desc())
-        .limit(num_games)
-        .all()
-    )
-
-    if not recent_points:
-        return [0.0, []]
-
-    total_points = sum(point[0] for point in recent_points)
-    average_points = round(total_points / num_games, 2)
-
-    return [average_points, [point[0] for point in recent_points]]
-
-
+    return average_and_recent_points(player_id, num_games, team_id)
 
 def getPointsByNumGames(player_id, num_games):
-    recent_points = (
-        db.session.query(PlayerStats.pts)
-        .join(PlayerStats.game)
-        .filter(PlayerStats.player_id == player_id)
-        .filter(PlayerStats.min != '00:00')
-        .filter(PlayerStats.min != '00')
-        .order_by(PlayerStats.game.date.desc())
-        .limit(num_games)
-        .all()
-    )
-
-    if not recent_points:
-        return [0.0, []]
-
-    total_points = sum(point[0] for point in recent_points)
-    average_points = round(total_points / num_games, 2)
-
-    return [average_points, [point[0] for point in recent_points]]
+    return average_and_recent_points(player_id, num_games)
