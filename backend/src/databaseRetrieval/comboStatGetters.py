56353c9b5f4ff481702d0e9930a1bffa6979a7fe
Taha Lewis <tahalewis@Tahas-Air.attlocal.net>
2023-10-19 17:30:34 -0500

    Retooled the PRA calculation and retrieval methods

--- backend/src/databaseRetrieval/comboStatGetters.py
+++ backend/src/databaseRetrieval/comboStatGetters.py
@@ -14,11 +14,11 @@ def getPRAComboForNumGames(player_id, num_games):
     pointsArr = getPointsByNumGames(player_id, num_games)
     reboundsArr = reboundsByNumGames(player_id, num_games)
     assistsArr = assistsByNumGames(player_id, num_games)
-    
-    pra_combo = [
-        point[0] + rebound[0] + assist[0]
-        for point, rebound, assist in zip(pointsArr, reboundsArr, assistsArr)
-    ]
+
+    pra_combo = []
+    for i in range(num_games):
+        pra_game = pointsArr[i] + reboundsArr[i] + assistsArr[i]
+        pra_combo.append(pra_game)
     
     average_PRA = round(sum(pra_combo) / num_games, 2)
     
@@ -30,11 +30,11 @@ def getPRAComboForNumGamesWithTeam(player_id, team_id, num_games):
     pointsArr = pointsByNumGames_teams(player_id, team_id, num_games)
     reboundsArr = reboundsByNumGames_team(player_id, team_id, num_games)
     assistsArr = assistsByNumGames_teams(player_id, team_id, num_games)
-    
-    pra_combo = [
-        point[0] + rebound[0] + assist[0]
-        for point, rebound, assist in zip(pointsArr, reboundsArr, assistsArr)
-    ]
+
+    pra_combo = []
+    for i in range(num_games):
+        pra_game = pointsArr[i] + reboundsArr[i] + assistsArr[i]
+        pra_combo.append(pra_game)
     
     average_PRA = round(sum(pra_combo) / num_games, 2)
     
