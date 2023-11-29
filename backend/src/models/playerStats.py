083b1f967b68ba99f1f1b9a00fe9be20822a4fb0
Taha Lewis <tahalewis@Tahas-Air.attlocal.net>
2023-11-28 20:22:12 -0600

    g

--- backend/src/models/playerStats.py
+++ backend/src/models/playerStats.py
@@ -42,9 +42,8 @@ class PlayerStats(db.Model):
 
     @staticmethod
     def fetch_and_insert_stats():
-        BASE_URL = "https://www.balldontlie.io/api/v1/stats?start_date={start}&end_date={end}"
+        BASE_URL = "https://www.balldontlie.io/api/v1/stats?start_date={start}"
         start = "2003-10-28"
-        end = "2023-11-27"
         PER_PAGE = 100  
 
         page = 1
