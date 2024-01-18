a9957673e310b722ae33e0ea24d635a962bcc3f5
Taha Lewis <tahalewis@Tahas-Air.attlocal.net>
2024-01-17 18:08:43 -0600

    Added new db struct

--- backend/src/models/game.py
+++ backend/src/models/game.py
@@ -41,7 +41,7 @@ class Game(db.Model):
         total_added = 0
 
         while True:
-            url = f"{BASE_URL}?per_page={PER_PAGE}&page={page}&seasons[]=2003"
+            url = f"{BASE_URL}?per_page={PER_PAGE}&page={page}&seasons[]=2002"
 
             try:
                 response = requests.get(url)
