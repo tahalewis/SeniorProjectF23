b82601fc4481b180c84781d0cd779e6b4ea564ab
garick-git <martin.mendez@erpchile.com>
2024-01-20 17:30:12 -0600

    more changes to render graph.

--- frontend/src/Graph.js
+++ frontend/src/Graph.js
@@ -1,35 +1,9 @@
-import React, { useEffect, useRef } from 'react';
-import * as d3 from 'd3';
-
 const Graph = ({ pointArray }) => {
-  const svgRef = useRef(null);
+    console.log('Hi! This is the graph component! The received point array is: ', pointArray);
 
-  useEffect(() => {
-    // D3.js code for rendering the graph
-    const svg = d3.select(svgRef.current);
-
-    // Example: Creating circles based on the pointArray
-    svg
-      .selectAll('circle')
-      .data(pointArray)
-      .enter()
-      .append('circle')
-      .attr('cx', (d) => d.x)
-      .attr('cy', (d) => d.y)
-      .attr('r', 5)
-      .attr('fill', 'blue');
-
-    // You can customize this code based on your specific use case
-
-  }, [pointArray]);
-
-  console.log('Hi! This is the graph component! The received point array is: ', pointArray);
-
-  return (
-    <div>
-      <svg ref={svgRef}></svg>
-    </div>
-  );
-};
+    return(
+        <div>Graph Component</div>
+    );
+}
 
 export default Graph;
