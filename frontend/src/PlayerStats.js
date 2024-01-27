e8ff85b4f0b70cd9349933722402ffc2fe9f44c2
garick-git <martin.mendez@erpchile.com>
2024-01-27 12:46:05 -0600

    working on displaying the graph

--- frontend/src/PlayerStats.js
+++ frontend/src/PlayerStats.js
@@ -649,7 +649,7 @@ const PlayerStats = () => {
       const changeDisplayedGraph = (currentCell) => {
         // 1 = points, 2 = free throws, 3 = rebounds, 4 = three pointers, 5 = assists, 6 = P+R+A
         console.log('cell clicked was ', currentCell)
-        // refreshStats(currentCell);
+        refreshStats(currentCell);
       }
 
       return (
