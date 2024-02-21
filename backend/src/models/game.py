3380492bd41bb143cfd93de6d8b44667b692b7b7
Taha Lewis <tahalewis@Tahas-Air.attlocal.net>
2024-02-20 22:50:26 -0600

    Pushed final games

--- backend/src/models/game.py
+++ backend/src/models/game.py
@@ -51,7 +51,7 @@ class Game(db.Model):
         seasons = [2023]
 
         while True:
-            url = f"{BASE_URL}?per_page={PER_PAGE}&page={page}&start_date=2023-07-01"
+            url = f"{BASE_URL}?per_page={PER_PAGE}&page={page}&seasons[]=2023"
 
             try:
                 response = requests.get(url)
